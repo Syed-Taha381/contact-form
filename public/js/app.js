4defbc5dbda53f7306bc62843ca5c043d0585c17
Syed-Taha381 <staha381@gmail.com>
2022-06-29 03:50:32 -0700

    origin

---
+++ public/js/app.js
@@ -0,0 +1,44 @@
+const contactForm = document.querySelector('.contact-form');
+let names = document.getElementById('name');
+let email = document.getElementById('email');
+let number = document.getElementById('number')
+let message = document.getElementById('message')
+let select = document.getElementById('ddlViewBy');
+select.addEventListener('change', function () {
+    console.log(select.value);
+});
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let formData = {
+        name: names.value,
+        email: email.value,
+        number: number.value,
+        select: select.value,
+        message: message.value
+    }
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/');
+    // xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+        if (xhr.responseText == 'success') {
+            alert('Email Send');
+            names.value = "";
+            email.value = "";
+            number.value = "";
+            select.value = "";
+            message.value = "";
+        } else {
+            alert('something went worong!')
+        }
+    }
+
+    xhr.send(JSON.stringify(formData));
+
+    console.log(formData)
+
+})
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const nodemailer = require("nodemailer")
+
+const PORT = process.env.PORT || 5000;
+
+//Middleware
+app.use(express.static('public'))
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + "/public/contactform.html")
+})
+
+app.post('/', (req, res)=>{
+    console.log(req.body)
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'staha381@gmail.com',
+            pass: 'xtwqemsslhwxuvps'
+            // user: process.env.USER,
+            // pass: process.env.PASS
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: "staha381@gmail.com",
+        subject: `Message from ${req.body.email}: ${req.body.select}`,
+        text: `${req.body.message} Contact me for more details ${req.body.number}`
+    }
+
+    transporter.sendMail(mailOptions, (error, info)=>{
+        if(error){
+            console.log(error);
+            res.send('error')
+        }else{
+            console.log('Email sent: ' + info.response)
+            res.send('Success')
+        }
+    })
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
